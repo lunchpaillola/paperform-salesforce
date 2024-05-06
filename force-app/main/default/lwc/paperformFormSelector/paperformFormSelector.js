cdeedbffdac7ccce462683184c89ee9f8e771e28
Lola-Ojabowale <lola.ojabowale@gmail.com>
2024-05-06 09:10:03 -0400

    deleting cli created apex classes

---
+++ force-app/main/default/lwc/paperformFormSelector/paperformFormSelector.js
@@ -0,0 +1,39 @@
+import { LightningElement, track } from 'lwc';
+import saveFormSettings from '@salesforce/apex/PaperformSettingsController.saveFormSettings';
+import getFormOptions from '@salesforce/apex/PaperformSettingsController.getFormOptions';
+
+export default class PaperformFormSelector extends LightningElement {
+    @track apiKey;
+    @track formId;
+    @track message;
+    @track formOptions = [];
+
+    handleApiKeyChange(event) {
+        this.apiKey = event.target.value;
+        // Fetch available forms based on the new API key
+        getFormOptions({ apiKey: this.apiKey })
+            .then((result) => {
+                this.formOptions = result.map((form) => ({
+                    label: form,
+                    value: form
+                }));
+            })
+            .catch((error) => {
+                this.message = 'Error fetching forms: ' + error.body.message;
+            });
+    }
+
+    handleFormSelection(event) {
+        this.formId = event.target.value;
+    }
+
+    handleSaveClick() {
+        saveFormSettings({ apiKey: this.apiKey, formId: this.formId })
+            .then((result) => {
+                this.message = result;
+            })
+            .catch((error) => {
+                this.message = 'Error saving settings: ' + error.body.message;
+            });
+    }
+}
